7c9af7e7ae266b5654028cd66efbdd9e739b8d89
armandsgarancs <Armands.Garancs@eschergroup.com>
2023-02-26 14:49:26 +0200

    Speed as arg when adding unit

--- game.js
+++ game.js
@@ -10,23 +10,21 @@ class GameRunner {
 		this.grid.init(this.scene);
 	}
 
-	addUnit(pos, initial_behaviour) {
-		let newUnit = new GameUnit(this.scene, pos)
+	addUnit(pos, speed, initial_behaviour) {
+		let newUnit = new GameUnit(this.scene, pos, speed)
 		if (initial_behaviour != undefined) {
 			newUnit.setBehaviour(initial_behaviour);
 		}
 		this.units.push(newUnit);
+		return newUnit
 	}
 
 	update(dt) {
 		this.grid.resetCells();
 		for (let i = 0; i < this.units.length; i++) {
 			this.units[i].update(dt);
-			//console.log(this.units[i])
 			let cell = this.grid.getCellAt(this.units[i].pos.x, this.units[i].pos.y)
 			cell.setHighlight(true)
-			//cell.highlight = true;
-			//cell.draw()
 			cell.process()
 		}
 	}
